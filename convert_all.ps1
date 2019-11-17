11856fef9d19ff82c91334798fdfee5164536570
Aendur <Aendur@users.noreply.github.com>
2019-11-17 07:37:16 -0300

    imporoved cvt script, fixed item 10

--- convert_all.ps1
+++ convert_all.ps1
@@ -1,11 +1,19 @@
-# & "D:\Program Files\LibreOffice\program\swriter.exe" --headless --convert-to pdf ".\1. Project Defined Process\Project Defined Process.rtf" 
 
+if ($args[0] -like "[0-1][0-9]*") {
+	$pattern = @($args[0])
+} else {
+	$pattern = ""
+}
 
 foreach ($folder in $(Get-ChildItem -Attributes Directory -Name .)) {
 	foreach ($file in $(Get-ChildItem -Name -Filter "*.rtf" $folder)) {
-		echo $folder\$file;
-		Start-Process -Wait "D:\Program Files\LibreOffice\program\swriter.exe" -ArgumentList "--headless --convert-to pdf `"$folder\$file`" --outdir `"$folder`" " 
+		if ($folder -like "$pattern*") {
+			echo $folder\$file;
+			Start-Process -Wait "D:\Program Files\LibreOffice\program\swriter.exe" -ArgumentList "--headless --convert-to pdf `"$folder\$file`" --outdir `"$folder`" " 
+		}
 	}
 }
 
-#  --outdir "$folder"'
+
+
+
